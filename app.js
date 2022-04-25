249a672156ccda564cf65978fb827bd673ad42d8
Mikhail Zapolskyi <mikhailzapolskyi@Mikhails-MacBook-Pro.local>
2022-04-24 21:10:17 -0600

    fixed 11 issue

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const text = document.querySelector('#text');
 const author = document.querySelector('#author');
 const button = document.querySelector('#new-quote');
+const tweet = document.querySelector('#tweet-quote');
 
 button.addEventListener('click', () => {
      fetch("https://type.fit/api/quotes")
@@ -11,5 +12,6 @@ button.addEventListener('click', () => {
           const quote = data[Math.floor(Math.random() * data.length)];
           text.textContent = quote.text;
           author.textContent = quote.author;
+          tweet.href = `https://twitter.com/intent/tweet?text=${quote.text} / ${ quote.author }`
      });
 });
